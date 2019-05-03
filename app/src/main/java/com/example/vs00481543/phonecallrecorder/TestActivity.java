a7b9445dd62e292fb238d6afc61de04f5ceaa77f
Amit <amit.g@mobinexttech.com>
2019-05-03 18:39:36 +0530

    First

---
+++ app/src/main/java/com/example/vs00481543/phonecallrecorder/TestActivity.java
@@ -0,0 +1,13 @@
+package com.example.vs00481543.phonecallrecorder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
